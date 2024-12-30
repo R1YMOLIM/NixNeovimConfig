18090814ee781b8ae25a2ff1ed5245717fa09e9d
Max <165298071+R1YMOLIM@users.noreply.github.com>
2024-12-30 18:27:09 +0200

    Add files via upload

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,69 @@
+
+local function default_header()
+    return {
+    "          ::::.    ':::::     ::::'          ",
+    "          ':::::    ':::::.  ::::'           ",
+    "            :::::     '::::.:::::            ",
+    "      .......:::::..... ::::::::             ",
+    "     ::::::::::::::::::. ::::::    ::::.     ",
+    "    ::::::::::::::::::::: :::::.  .::::'     ",
+    "           .....           ::::' :::::'      ",
+    "          :::::            '::' :::::'       ",
+    " ........:::::               ' :::::::::::.  ",
+    ":::::::::::::                 :::::::::::::  ",
+    " ::::::::::: ..              :::::           ",
+    "     .::::: .:::            :::::            ",
+    "    .:::::  :::::          '''''    .....    ",
+    "    :::::   ':::::.  ......:::::::::::::'    ",
+    "     :::     ::::::. ':::::::::::::::::'     ",
+    "            .:::::::: '::::::::::            ",
+    "           .::::''::::.     '::::.           ",
+    "          .::::'   ::::.     '::::.          ",
+    "         .::::      ::::      '::::.         ",
+    "                                             ",
+    "                                             ",
+   }
+end
+
+return {
+  'nvimdev/dashboard-nvim',
+  config = function()
+    require('dashboard').setup {
+      theme = 'doom',
+      config = {
+        header = default_header(),
+        center = {
+          {
+            desc = '󰊳 Update',
+            group = '@property',
+            action = 'Lazy update',
+            key = 'u'
+          },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            icon = ' ',
+            desc = 'Config',
+            action = 'edit ~/.config/nvim/init.lua',
+            key = 'c'
+          },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Recent Files',
+            group = 'Label',
+            action = 'Telescope oldfiles',
+            key = 'r',
+          },
+        },
+      },
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
